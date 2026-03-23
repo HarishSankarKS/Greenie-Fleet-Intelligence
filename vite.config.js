54d746765c816304378c6c00db2f5e403fa31742
727723eucv021-web <727723eucv021@skcet.ac.in>
2026-03-23 16:49:18 +0530

    fix: github pages deployment settings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Greenie-Fleet-Intelligence/',
+  base: '/Greenie Fleet Intelligence/',
   plugins: [react(), tailwindcss()],
 })
